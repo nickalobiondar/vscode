d0502447e85b8c23572561515bcd201cd28f87f0
nickalobiondar <23122181+nickalobiondar@users.noreply.github.com>
2018-12-04 11:07:00 +0700

    refactor: tcp proxy

--- go/internal/capture/proxy.go
+++ go/internal/capture/proxy.go
@@ -12,3 +12,18 @@ import (
 	"time"
 
 	"github.com/portsmith/portcap/internal/trace"
+)
+
+// Proxy accepts connections on Listen, dials Target, and copies bytes in both
+// directions while writing each chunk to a trace.Writer.
+type Proxy struct {
+	Listen string
+	Target string
+	Proto  string
+
+	mu     sync.Mutex
+	writer *trace.Writer
+	now    func() time.Time
+}
+
+// New creates a Proxy that writes captured records via w.
