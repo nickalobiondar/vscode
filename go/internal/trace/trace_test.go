b6a3901c7fa870b99716f39b0d7170a073df8600
nickalobiondar <23122181+nickalobiondar@users.noreply.github.com>
2019-01-22 09:21:00 +0700

    feat: add tcp proxy

--- go/internal/trace/trace_test.go
+++ go/internal/trace/trace_test.go
@@ -4,3 +4,13 @@ import (
 	"bytes"
 	"io"
 	"testing"
+)
+
+func TestEncodeDecodeRoundTrip(t *testing.T) {
+	recs := []Record{
+		{TimestampNanos: 1000, Dir: Request, Session: "abc123", Proto: "redis", Payload: []byte("PING\r\n")},
+		{TimestampNanos: 2000, Dir: Response, Session: "abc123", Proto: "redis", Payload: []byte("+PONG\r\n")},
+		{TimestampNanos: 3000, Dir: Request, Session: "abc123", Proto: "raw", Payload: []byte{0x00, 0xff, 0x10}},
+	}
+
+	var buf bytes.Buffer
