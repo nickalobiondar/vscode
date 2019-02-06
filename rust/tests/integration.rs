af1a1e1c8428388c9fd6aa19375463467d6e69ad
nickalobiondar <23122181+nickalobiondar@users.noreply.github.com>
2019-02-06 10:27:00 +0700

    feat: traffic capture

---
+++ rust/tests/integration.rs
@@ -0,0 +1,9 @@
+use portsmith_replay::schema::{infer, Encoding};
+use portsmith_replay::trace::{base64_decode, base64_encode, decode_line, Direction, Record};
+
+#[test]
+fn base64_round_trips() {
+    let cases: &[&[u8]] = &[
+        b"",
+        b"f",
+        b"fo",
