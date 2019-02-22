de83ab9ad9cb7b238fa24c679d2202d266a0eafd
nickalobiondar <23122181+nickalobiondar@users.noreply.github.com>
2019-02-22 14:15:00 +0700

    chore: replay diff

--- rust/tests/integration.rs
+++ rust/tests/integration.rs
@@ -7,3 +7,13 @@ fn base64_round_trips() {
         b"",
         b"f",
         b"fo",
+        b"foo",
+        b"foob",
+        b"fooba",
+        b"foobar",
+        &[0, 255, 16, 200],
+    ];
+    for c in cases {
+        let enc = base64_encode(c);
+        let dec = base64_decode(&enc).expect("decode");
+        assert_eq!(&dec, c, "round trip failed for {c:?}");
